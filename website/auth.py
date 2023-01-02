760bf053c4fe6ccf24e057c9c95f40653552fca5
doughnut187 <tjoyce@uoregon.edu>
2023-01-02 13:37:34 -0800

    fixed signup bug

--- website/auth.py
+++ website/auth.py
@@ -54,8 +54,8 @@ def sign_up():
             new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            # print(new_user)
-            login_user(user, remember=True)
+            print(new_user)
+            login_user(new_user, remember=True)
             flash('Account created!', category='success')
             return redirect(url_for('views.home'))
             
