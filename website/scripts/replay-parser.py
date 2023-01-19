385f07f0c60e30866e7af93351f4fca5e3589c0b
Nik Cotta <nikcotta@gmail.com>
2023-01-19 15:28:18 -0800

    implemented replay parser to extract .replay file data

--- website/scripts/replay-parser.py
+++ website/scripts/replay-parser.py
@@ -1,9 +1,29 @@
 """
-Series: Best of 5
-When people upload scores, will be uploading 3-5 replay files
-Will need to parse all info, then get rid of files so we don't keep themgit 
-Queue system for uploads (FIFO)
+Author: Niklaas Cotta
+Last Updated: 1/19/23
 """
 
 import carball
+import json
 
+def extractReplayFile(pathToInput, pathToOutput):
+    """
+    Extracts relevant data from replay file and exports into .json
+    """
+    matchDataDict = carball.decompile_replay(pathToInput)
+
+    extractedData = {
+        "matchID": matchDataDict['properties']['Id'],
+        "team0Score": matchDataDict['properties']['Team0Score'],
+        "team1Score": matchDataDict['properties']['Team1Score'],
+        "playerStats": matchDataDict['properties']['PlayerStats']
+    }
+
+    matchDataJSON = json.dumps(extractedData, indent=4)
+
+    with open(pathToOutput, "w") as outfile:
+        outfile.write(matchDataJSON)
+
+
+# Example of use
+extractReplayFile("./match.replay", "./website/data/match.json")
