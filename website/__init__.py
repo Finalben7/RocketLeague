caaf6d71ac25d624ab1f143637b992f7467ed5c3
tjoyce <tjoyce@uoregon.edu>
2023-01-24 14:26:47 -0800

    meh

--- website/__init__.py
+++ website/__init__.py
@@ -2,17 +2,38 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+from flask_mysqldb import MySQL
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     
     app.config['SECRET_KEY'] = 'secretkeytest'
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_NAME
+    # MySQL credentials
+    app.config['SECRET_KEY'] = 'secretkeytest'  # needed?
+    app.config['MYSQL_HOST'] = ''  #FIXME
+    app.config['MYSQL_USER'] = ''  #FIXME
+    app.config['MYSQL_PASSWORD'] = ''  #FIXME
+    app.config['MYSQL_DB'] = DB_NAME  #FIXME
+    # app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + DB_NAME
+
+    mysql = MySQL(app)
+
+    """
+    Example code for SQL statements
+
+    cursor = mysql.connection.cursor()  # Create connection cursor so Flask can interact with tables
+    cursor.execute(''' CREATE TABLE table_name(field1, field2...) ''')
+    cursor.execute(''' INSERT INTO table_name VALUES(v1,v2...) ''')
+    cursor.execute(''' DELETE FROM table_name WHERE condition ''')
+
+    mysql.connection.commit()  # save actions performed
+    cursor.close()  # disconnect
+    """
     
-    db.init_app(app)
+    # db.init_app(app)
     
     from .views import views
     from .auth import auth
@@ -22,8 +43,8 @@ def create_app():
     
     from .models import User, Note
     
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    #     db.create_all()
     
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
