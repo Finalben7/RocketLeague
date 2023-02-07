5d4cced5fb4b061ce200d51cbb58091da2279368
Nik Cotta <nikcotta@gmail.com>
2023-02-07 11:04:37 -0800

    minor changes to message system

--- website/message_portal.py
+++ website/message_portal.py
@@ -1,6 +1,30 @@
 """
 Author: Niklaas Cotta
-Last Updated: 1/24/23
+Last Updated: 2/2/23
+Base: https://www.youtube.com/watch?v=whEObh8waxg
+Tunnelling: https://www.youtube.com/watch?v=hs7GLsHQCPQ
+Matchmaking: https://youtu.be/_08lsRxqnm4
 """
 
-# https://www.youtube.com/watch?v=whEObh8waxg
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET'] = "secret!123"  # FIXME
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on('message')
+def handle_message(message):
+    print(f"Recieved message: {message}")
+    if message != "User connected!":
+        send(message, broadcast=True) 
+
+
+@app.route('/match')
+def index():
+    return render_template("match.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, host="localhost")
