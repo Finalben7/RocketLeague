3cc60f8b123b100e910abb386398b158222f07f5
Finalben7 <kauffman4323@hotmail.com>
2023-03-07 12:22:39 -0800

    Working on team.html for queue button

--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
 from .models import User, Team, TeamPlayers
 from . import db
@@ -17,9 +17,11 @@ def profile():
 
 @views.route('/teams')
 def teams():
-    current_user_id = current_user.id  # Replace with your current user ID
+    #Create a variable to store current_users id for use in query
+    current_user_id = current_user.id
+    #Find all usernames from each teamId associated with the current_user.id's teamId's (that's a mouthful)
     query = f'''
-        SELECT u.username, t.teamName, t.rank, t.region
+        SELECT u.username, t.teamName
         FROM User u
         INNER JOIN TeamPlayers tp ON u.id = tp.userId
         INNER JOIN Team t ON tp.teamId = t.id
@@ -32,8 +34,27 @@ def teams():
     with db.engine.connect() as con:
         result = con.execute(query)
         teams = result.fetchall()
-    print(teams)
-    return render_template('teams.html', user=current_user, teams=teams)
+        #Store the results of the query in a dictionary to make data easier to access with Jinja
+        team_users = {}
+        for team in teams:
+            team_name = team['teamName']
+            if team_name not in team_users:
+                team_users[team_name] = []
+            team_users[team_name].append(team['username'])
+    return render_template('teams.html', user=current_user, teams=team_users)
+
+@views.route('/team')
+def team():
+    team_id = request.args.get('team_id')
+    print(team_id)
+    # Fetch the team object from the database using the team_id
+    #team = Team.query.filter_by(id=team_id).first()
+    #print(team)
+    # Fetch the players for the team
+    #players = User.query.join(TeamPlayers).filter(TeamPlayers.teamId == team.id).all()
+    #print(players)
+    # Render the team page template and pass in the team and players objects
+    return render_template('team.html', user=current_user, team_id=team_id)
 
 @views.route('/match')
 def match():
