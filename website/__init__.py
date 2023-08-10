ba28aa58ec8d4234422dc2f5cd6ac5d533b11f6f
Finalben7 <kauffman4323@hotmail.com>
2023-08-10 14:29:36 -0700

    Version 1.5 Password Reset

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mysqldb import MySQL
 from flask_uploads import IMAGES, UploadSet, configure_uploads
+from flask_mail import Mail
 from . import glblvars
 
 db = SQLAlchemy()
@@ -10,13 +11,16 @@ db = SQLAlchemy()
 # For profile pictures
 images = UploadSet('images', IMAGES)
 
+# For password reset
+mail = Mail()
+
 def create_app():
     app = Flask(__name__)
     mysql = MySQL(app)
 
-    MYSQL_HOST = glblvars.DB_HOST  #FIXME
+    MYSQL_HOST = glblvars.DB_HOST
     MYSQL_USER = glblvars.DB_USER
-    MYSQL_PASSWORD = glblvars.DB_PASS  #FIXME
+    MYSQL_PASSWORD = glblvars.DB_PASS
     SECRET_KEY = glblvars.SECRET_KEY
     MYSQL_PORT = glblvars.DB_PORT
     MYSQL_NAME = glblvars.DB_NAME
@@ -30,8 +34,17 @@ def create_app():
     
     db.init_app(app)
 
+    # Configure app to handle image upload
     app.config["UPLOADED_IMAGES_DEST"] = "website/static/uploads"
     configure_uploads(app, images)
+
+    # Configure app for email to allow users to reset passwords
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USERNAME'] = "eleventhhouresports@gmail.com"
+    app.config['MAIL_PASSWORD'] = "cjscyascqlysncvs"
+    mail = Mail(app)
 
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,27 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
-from . import db
+from . import db, mail
 from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy import or_
-from datetime import datetime, timedelta
-
+from flask_mail import Message
+import jwt
+from flask import current_app
 
 auth = Blueprint('auth', __name__)
+
+################# Function to verift JWT token passed as an argument from resetEmail.html #################
+def verifyResetToken(token):
+    try:
+        secret_key = current_app.config['SECRET_KEY']
+        decoded_token = jwt.decode(token, key=secret_key, algorithms=['HS256'])
+        username = decoded_token['reset_password']
+        print(secret_key, decoded_token, username)
+    except jwt.exceptions.DecodeError:
+        flash('Invalid or expired reset token.', category='error')
+        return render_template('login.html', user=current_user)
+    return User.query.filter_by(username=username).first()
+
 
 ################# Login #################
 @auth.route('/login', methods=['GET', 'POST'])
@@ -71,10 +85,9 @@ def sign_up():
                        
     return render_template('signup.html', user=current_user)
 
-################# Reset Password #################
-@auth.route('/resetPassword', methods=['GET', 'POST'])
-def resetPassword():
-    print("Reset Password Has Been Called!")
+################# Request Email To Reset Password #################
+@auth.route('/requestPasswordReset', methods=['GET', 'POST'])
+def requestPasswordReset():
     if request.method == 'POST':
         # Get email address entered into form
         email = request.form.get('email')
@@ -83,13 +96,51 @@ def resetPassword():
         user = User.query.filter(User.email == email).first()
 
         if user:
-            reset_token = generate_password_hash(user.email + str(datetime.now()))
-            reset_token_expiration = datetime.now() + timedelta(hours=1)
-            db.session.commit()
-            print(reset_token)
-            print(reset_token_expiration)
+
+            token = jwt.encode({'reset_password': user.username}, key = current_app.config['SECRET_KEY'])
+
+            msg = Message()
+            msg.subject = "Reset your Underground account's password"
+            msg.recipients = [user.email]
+            msg.sender = 'eleventhhouresports@gmail.com'
+            msg.html = render_template('resetEmail.html', user=user, token=token)
+
+            mail.send(msg)
+            flash(f"Email has been sent to {user.email}, please follow the link provided to reset your password.", category='success')
+            return render_template('requestPasswordReset.html', user=current_user)
         else:
             flash('Email address not found.', category='error')
-            return redirect(url_for('resetPassword.html'))
+            return render_template('requestPasswordReset.html', user=current_user)
     
-    return render_template('resetPassword.html')
+    return render_template('requestPasswordReset.html', user=current_user)
+
+################# Verification Email #################
+@auth.route('/resetEmail', methods=['GET', 'POST'])
+def resetEmail():
+
+    return render_template('resetEmail.html', user=current_user)
+
+@auth.route('/resetVerified/<token>', methods=['GET', 'POST'])
+def resetVerified(token):
+    
+    user = verifyResetToken(token)
+    print(user.username)
+
+    if not user:
+        print('No User Found!')
+        flash('Email address not found.', category='error')
+        return redirect(url_for('auth.login'))
+
+    newPassword = request.form.get('password')
+    if newPassword:
+        if len(newPassword) < 6:
+            flash("Password must be greater than 6 characters", category='error')
+        else:
+            print(newPassword)
+            user.password = generate_password_hash(newPassword, method='sha256')
+            db.session.commit()
+            flash('Password successfully updated.', category='success')
+            return redirect(url_for('auth.login'))
+    
+    flash('Reset token verified, please enter a new password.', category='success')
+    return render_template('resetVerified.html', user=current_user)
